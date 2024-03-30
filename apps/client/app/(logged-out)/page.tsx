0201de4252e514ca8d5d14839b9d4a1339fac9d1
Kam <kamran.programing@gmail.com>
2024-03-30 22:03:20 +0000

    add logged-out route group with layout, page for the landing page

---
+++ apps/client/app/(logged-out)/page.tsx
@@ -0,0 +1,19 @@
+import { Button } from "@repo/ui/components/ui/button";
+import { PersonStandingIcon } from "@repo/ui/components/ui/icons";
+import Link from "next/link";
+
+export default function LandingPage() {
+  return (
+    <>
+      <h1 className="flex gap-2 items-center">
+        <PersonStandingIcon size={50} className="text-pink-500" /> Sleep Logger
+      </h1>
+      <p>Start logging sleeps</p>
+      <div className="flex gap-2 items-center">
+        <Button asChild>
+          <Link href="/sleeps-overview">Go</Link>
+        </Button>
+      </div>
+    </>
+  );
+}
